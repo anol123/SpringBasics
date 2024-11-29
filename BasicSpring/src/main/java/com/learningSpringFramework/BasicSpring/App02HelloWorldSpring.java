1e767e2b156a5fb9f32a8d20dc941447a210fb4e
anol123 <anol.roy123@gmail.com>
2024-11-29 12:20:49 +0530

    Created the beans and added customised names to it

--- BasicSpring/src/main/java/com/learningSpringFramework/BasicSpring/App02HelloWorldSpring.java
+++ BasicSpring/src/main/java/com/learningSpringFramework/BasicSpring/App02HelloWorldSpring.java
@@ -7,6 +7,8 @@ public class App02HelloWorldSpring {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
         System.out.println(context.getBean("name"));
+        System.out.println(context.getBean("age"));
+        System.out.println(context.getBean("person_anol")); //customised the naming of bean
     }
 
 }
--- BasicSpring/src/main/java/com/learningSpringFramework/BasicSpring/HelloWorldConfiguration.java
+++ BasicSpring/src/main/java/com/learningSpringFramework/BasicSpring/HelloWorldConfiguration.java
@@ -4,11 +4,27 @@ package com.learningSpringFramework.BasicSpring;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person(String name, int age){};
+record Address(String colonyNAme, int h_no){};
+
 @Configuration
 public class HelloWorldConfiguration {
     @Bean
     public String name(){
         return "Anol";
     }
+
+    @Bean
+    public int age(){
+        return 26;
+    }
+
+    @Bean("person_anol")    //customised the bean name
+    public Person person1 (){
+        var person1 = new Person("Anol", 26);
+        return person1;
+    }
+
+
 
 }
